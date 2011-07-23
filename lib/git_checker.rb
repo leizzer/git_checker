e3c67187ce6835b6e66f4b0831336e363fe97bb7
Cristian <leizzer@bt.foo.org>
2011-07-22 22:52:03 -0400

    printing headers when calling a method

--- lib/git_checker.rb
+++ lib/git_checker.rb
@@ -5,26 +5,34 @@ require_relative 'branch'
 
 module GitChecker
 
-  def compare_branches_between_repos target_repo, repo_to_compare, ignore=[]
-    puts target_repo.missing_branches(repo_to_compare.list_of_branches) - ignore
+  include MessagePrinter
+
+  #def compare_branches_between_repos target_repo, repo_to_compare, ignore=[]
+  def branches_not_in repo, from, ignore=[]
+    print_message "Branches in #{from.name.capitalize} not in #{repo.name.capitalize}"
+    puts repo.missing_branches(from.list_of_branches) - ignore
   end
 
-  def check_if_branches_are_merged_in target_branch, repo_to_check, ignore=[]
+  #def check_if_branches_are_merged_in target_branch, repo_to_check, ignore=[]
+  def branches_not_merged_in branch, from, ignore=[]
+    print_message "Branches not in #{branch.name.capitalize}"
     branches = []
-    repo_to_check.branches.values.each do |b|
+    from.branches.values.each do |b|
       unless ignore.include? b.name
-        branches << b.name if target_branch.merges_with(b.name).empty?
+        branches << b.name if branch.merges_with(b.name).empty?
       end
     end
     puts branches
   end
 
-  def check_what_commits_not_merged_with target_branch, repo_to_check, ignore=[]
-    repo_to_check.branches.each_value do |b|
+  #def check_what_commits_not_merged_with target_branch, repo_to_check, ignore=[]
+  def commits_not_in branch, from, ignore=[]
+    print_message "Commits not in #{branch.name.capitalize}"
+    from.branches.each_value do |b|
       unless ignore.include? b.name
-        log = target_branch.commits_not_merged_from b
+        log = branch.commits_not_merged_from b
         unless log.empty?
-          puts "\n\n\e[1m\e[31m\e[7m++++ #{b.name}: Commits not on Master ++++\e[m\e[m\e[m"
+          print_message "#{b.name}: Commits not on #{branch.name.capitalize}", :color => "\e[31m", :pound => '+'
           puts log
         end
       end
